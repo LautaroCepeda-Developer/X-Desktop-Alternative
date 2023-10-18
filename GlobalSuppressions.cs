33b8c4dcb6b89e253e191466db25b9a49a422112
LautaroCepeda-Developer <123782531+LautaroCepeda-Developer@users.noreply.github.com>
2023-10-18 16:41:54 -0300

    Delete GlobalSuppressions.cs

--- GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Performance", "CA1822:Marcar miembros como static", Justification = "<pendiente>", Scope = "member", Target = "~M:X_Desktop_Alternative.Models.ThemesModel.HexToBrush(System.String)~System.Windows.Media.Brush")]
